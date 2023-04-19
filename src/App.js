f49981fb5a3379ef0b8a60ef32ddfc4f2c8a9e6d
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-20 00:47:26 +0300

    props with Object

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
-import Counter from "./components/Counter";
-import ClassCounter from './components/ClassCounter';
 import "./styles/myStyle.css";
-import PostItem from './components/PostItem';
-
+import PostItem from "./components/PostItem"
 
 function App() {
-  // const [value, setValue] = useState("some text in input");
+  const [posts, setPosts] = useState( [
+    { id: 1, title: "JS", body: "Description" },
+    { id: 2, title: "PHP", body: "Description" },
+    { id: 3, title: "Java", body: "Description" },
+    { id: 4, title: "Python", body: "Description" },
+
+  ])
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'JS', body: 'Description'}}/>
-      <PostItem post={{id: 2, title: 'PHP', body: 'Description'}}/>
-      <PostItem post={{id: 3, title: 'Java', body: 'Description'}}/>
-      <PostItem post={{id: 4, title: 'Python', body: 'Description'}}/>
-
+      {posts.map((post) =>
+          <PostItem post={post} key={post.id} />
+       )}
     </div>
   );
 }
