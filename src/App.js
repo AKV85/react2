6254c92e94670435086d189b10f02fe6ed3ae398
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-21 00:10:45 +0300

    sort

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import PostList from './components/PostList';
 // import MyButton from './components/UI/buttons/MyButton';
 // import MyInput from './components/UI/inputs/MyInput';
 import PostForm from './components/PostForm';
-
+import MySelect from './components/UI/selects/MySelect';
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "JS", body: "Description" },
@@ -14,25 +14,38 @@ function App() {
     { id: 4, title: "Python", body: "Description" },
   ])
 
-const createPost =(newPost) => {
-  setPosts([...posts, newPost])
-}
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
 
-//had post from children component
-const removePost = (post) => {
-  setPosts(posts.filter(p => p.id !== post.id))
-}
-
+  //had post from children component
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
   return (
     <div className="App">
       <PostForm create={createPost} />
-      {posts.length !== 0}
-      ? <PostList remove={removePost} posts={posts} title={"Posts list-1"} />
-      : <div>No posts find</div>
+      <hr style={{margin: "15px 0" }} />
+      <div>
+        <MySelect
+          defaultValue="Sort"
+          options={[
+            {value: 'title', name: "By name"},
+            {value: 'body', name: "By description"}
 
+          ]}
+        />
 
-
-
+        
+      </div>
+      {posts.length !== 0
+        ?
+        <PostList remove={removePost} posts={posts} title={"Posts list-1"} />
+        :
+        <h1 style={{ textAlign: 'center' }}>
+          No posts find
+        </h1>
+      }
     </div>
   )
 }
