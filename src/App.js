2ee4a91382154865a08a461636edbfa91b175d70
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-20 14:02:59 +0300

    props2

--- src/App.js
+++ src/App.js
@@ -9,12 +9,27 @@ function App() {
     { id: 2, title: "PHP", body: "Description" },
     { id: 3, title: "Java", body: "Description" },
     { id: 4, title: "Python", body: "Description" },
-
+  ])
+  const [posts2, setPosts2] = useState( [
+    { id: 1, title: "CSS", body: "Description" },
+    { id: 2, title: "PHP", body: "Description" },
+    { id: 3, title: "HTML", body: "Description" },
+    { id: 4, title: "Python", body: "Description" },
+  ])
+  const [posts3, setPosts3] = useState( [
+    { id: 1, title: "JS", body: "Description" },
+    { id: 2, title: "PHP", body: "Description" },
+    { id: 3, title: "Django", body: "Description" },
+    { id: 4, title: "Python", body: "Description" },
   ])
 
   return (
     <div className="App">
       <PostList posts={posts} title={"Posts list-1"}/>
+      <PostList posts={posts2} title={"Posts list-2"}/>
+      <PostList posts={posts3} title={"Posts list-3"}/>
+
+
     </div>
   )}
 export default App;
