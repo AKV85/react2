7484d9399164034231f2c8756589062b68b7dab8
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-20 15:36:33 +0300

    MyInput component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/myStyle.css";
 import PostItem from "./components/PostItem"
 import PostList from './components/PostList';
 import MyButton from './components/UI/buttons/MyButton';
+import MyInput from './components/UI/inputs/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -15,9 +16,9 @@ function App() {
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="post name" />
-        <input type="text" placeholder="Post description" />
-        <MyButton disabled>
+        <MyInput type="text" placeholder="Post name" />
+        <MyInput type="text" placeholder="Post description" />
+        <MyButton>
           Create post
         </MyButton>
       </form>
