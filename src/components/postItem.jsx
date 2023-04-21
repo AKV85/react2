e5fdd002e16a7ddab2128af43d404f72f441396f
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-21 12:18:46 +0300

    MyModal visible-unvisible

--- src/components/postItem.jsx
+++ src/components/postItem.jsx
@@ -7,15 +7,15 @@ const PostItem = (props) => {
       <div className="post__content">
         <strong>{props.number}. {props.post.title}</strong>
         <div>
-        {props.post.body}
+          {props.post.body}
         </div>
       </div>
-        <div className="post__btns">
-        <MyButton onClick={() => props.remove(props.post)}> 
-                Delete
-          </MyButton>
+      <div className="post__btns">
+        <MyButton onClick={() => props.remove(props.post)}>
+          Delete
+        </MyButton>
       </div>
-    </div> 
-  ); 
-  } 
+    </div>
+  );
+}
 export default PostItem;
