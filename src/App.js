a03d64194c7abbb57094ad5781e4638a3d1b91ef
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-23 23:13:03 +0300

    comments and email from fake server

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import "./styles/myStyle.css";
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
-
 import Navbar from './components/UI/navbars/Navbar';
 
 function App() {
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,17 +7,21 @@ import Loader from '../components/UI/loaders/Loader';
 const PostIdPage = () => {
     const params = useParams()
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id)
         setPost(response.data);
     })
     const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
-        const response = await PostService.getById(id)
-        setPost(response.data);
+        const response = await PostService.getCommentsByPostId(id)
+        setComments(response.data);
     })
 
     useEffect(() => {
         fetchPostById(params.id)
+        fetchComments(params.id)
+
     })
     return (
         <div>
@@ -29,6 +33,22 @@ const PostIdPage = () => {
             <h1>
                 Comments
             </h1>
+            {isComLoading
+                ?
+                <Loader />
+                :
+                <div>{comments.map(comm =>
+                    <div style={{ marginTop: '15px' }}>
+                        <h5>
+                            {comm.email}
+                        </h5>
+                        <div>
+                            {comm.body}
+                        </div>
+                    </div>
+                )}
+                </div>
+            }
         </div>
     );
 }
