73cb9c88ec9152b359d6b993003c54cc936bc9cc
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-23 22:21:04 +0300

    PostIdPage(edit)

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useFetching } from '../components/hooks/UseFetching';
+import PostService from '../API/PostService';
+import Loader from '../components/UI/loaders/Loader';
 
 const PostIdPage = () => {
     const params = useParams()
-    console.log(params)
-    return ( 
+    const [post, setPost] = useState({});
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(id)
+        setPost(response.data);
+    })
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getById(id)
+        setPost(response.data);
+    })
+
+    useEffect(() => {
+        fetchPostById(params.id)
+    })
+    return (
         <div>
             <h1>You are on post Id page with ID = {params.id}</h1>
+            {isLoading
+                ? <Loader />
+                : <div>{post.id}. {post.title}</div>
+            }
+            <h1>
+                Comments
+            </h1>
         </div>
-     );
+    );
 }
- 
+
 export default PostIdPage;
