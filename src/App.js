3672eb301a6bf178233a88ab2646d05708605ab9
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-24 02:03:45 +0300

    localStorage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./styles/myStyle.css";
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/UI/navbars/Navbar';
 import { AuthContext } from './context';
-import { useState } from 'react';
 
 function App() {
-  const [isAuth,setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+      setIsAuth(true)
+    }
+  }, [])
+
   return (
     <AuthContext.Provider value={{
       isAuth,
       setIsAuth
     }}>
- <BrowserRouter>
-      <Navbar />
-      <AppRouter />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <AppRouter />
+      </BrowserRouter>
     </AuthContext.Provider>
-   
+
   )
 }
 
--- src/components/UI/navbars/Navbar.jsx
+++ src/components/UI/navbars/Navbar.jsx
@@ -4,18 +4,24 @@ import MyButton from '../buttons/MyButton';
 import { AuthContext } from '../../../context';
 
 const Navbar = () => {
-  const {isAuth,setIsAuth} = useContext(AuthContext)
-    return (
-        <div className="navbar">
-          <MyButton onClick={() =>setIsAuth(false)}>
-            Log Out
-          </MyButton>
-        <div className="navbar__links">
-          <Link to="/about">About site</Link>
-          <Link to="/posts">Posts</Link>
-        </div>
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+
+  const logout = () => {
+    setIsAuth(false);
+    localStorage.removeItem('auth')
+  }
+
+  return (
+    <div className="navbar">
+      <MyButton onClick={logout}>
+        Log Out
+      </MyButton>
+      <div className="navbar__links">
+        <Link to="/about">About site</Link>
+        <Link to="/posts">Posts</Link>
       </div>
-      );
+    </div>
+  );
 }
- 
+
 export default Navbar;
