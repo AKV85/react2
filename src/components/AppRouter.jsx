95113a82716ec730081bd0874244f91cfd37f219
AKV85 <a.kotov.laknojus@gmail.com>
2023-04-23 18:03:53 +0300

    PostIdPage

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,13 +3,15 @@ import { Navigate, Routes, Route } from 'react-router-dom';
 import About from '../pages/About';
 import Posts from '../pages/Posts';
 import Error from '../pages/Error';
+import PostIdPage from '../pages/PostIdPage';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<Posts />} />
+            <Route exact path="/posts" element={<Posts />} />
             <Route path="/error" element={<Error />} />
+            <Route exact path="/posts/:id" element={<PostIdPage />} />
             <Route path="*" element={<Navigate to="/error" />} />
         </Routes>
     );
---
+++ src/pages/PostIdPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostIdPage = () => {
+    return ( 
+        <div>
+            <h1>You are on post Id page</h1>
+        </div>
+     );
+}
+ 
+export default PostIdPage;
